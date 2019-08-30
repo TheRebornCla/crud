29eaea25dd8f113aba40e8615e4cc618b73adfc9
lucasborbaa <54078837+lucasborbaa@users.noreply.github.com>
2019-08-30 14:41:19 -0300

    kerogames.sql && conexao.php update

--- conexao.php
+++ conexao.php
@@ -2,7 +2,7 @@
 	$servidor = "localhost";
 	$usuario = "root";
 	$senha = "";
-	$dbname = "tablegames";
+	$dbname = "kerogames";
 	
 	//Criar a conexão
 	$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
--- kerogames.sql
+++ kerogames.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 30-Ago-2019 às 04:41
--- Versão do servidor: 10.4.6-MariaDB
--- versão do PHP: 7.3.8
+-- Generation Time: 30-Ago-2019 às 19:39
+-- Versão do servidor: 10.1.37-MariaDB
+-- versão do PHP: 7.3.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,8 +19,29 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `tablegames`
+-- Database: `kerogames`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `accounts`
+--
+
+CREATE TABLE `accounts` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `accounts`
+--
+
+INSERT INTO `accounts` (`id`, `username`, `password`, `email`) VALUES
+(1, 'anderson', '$2y$12$0jKixJNHdmQOd04/chxh0OKp5pLglV9sm1kc0EMl7BJ29vCPAlxpy', 'andersonhitchcock@gmail.com'),
+(2, 'ceica', '$2y$12$0jKixJNHdmQOd04/chxh0OKp5pLglV9sm1kc0EMl7BJ29vCPAlxpy', 'ceicaribeiro@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -50,21 +71,33 @@ INSERT INTO `jogos` (`ordem`, `plataforma`, `jogo`, `login`, `senha`, `id`, `ace
 (2, 'PS4', 'mortal combat 3', 'lucaswallyson@gmail.com', '1234567', 'XVCFDSZ', 'XCVSDF e BNMGHJ', '', '2019-12-09', 'joceliano peixoto', '20,00');
 
 --
--- Índices para tabelas despejadas
+-- Indexes for dumped tables
 --
 
 --
--- Índices para tabela `jogos`
+-- Indexes for table `accounts`
+--
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `jogos`
 --
 ALTER TABLE `jogos`
   ADD PRIMARY KEY (`ordem`);
 
 --
--- AUTO_INCREMENT de tabelas despejadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de tabela `jogos`
+-- AUTO_INCREMENT for table `accounts`
+--
+ALTER TABLE `accounts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `jogos`
 --
 ALTER TABLE `jogos`
   MODIFY `ordem` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
